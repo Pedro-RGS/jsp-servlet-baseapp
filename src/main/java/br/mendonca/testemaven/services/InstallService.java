db328f42c91069c12466b369480483643b087169
paulabeatrizz <paulinhabeatrizcontato@gmail.com>
2024-11-05 15:41:33 -0300

    feat:adição de método para criar tabela Chef

--- src/main/java/br/mendonca/testemaven/services/InstallService.java
+++ src/main/java/br/mendonca/testemaven/services/InstallService.java
@@ -24,6 +24,10 @@ public class InstallService {
 	public void deleteUserTable() throws ClassNotFoundException, SQLException {
 		statement("DROP TABLE IF EXISTS users");
 	}
+
+	public void deleteChefTable() throws ClassNotFoundException, SQLException {
+		statement("DROP TABLE IF EXISTS chefs");
+	}
 	
 	public void createUserTable() throws ClassNotFoundException, SQLException {
 		statement("CREATE TABLE users ("
@@ -31,5 +35,13 @@ public class InstallService {
 					+ "    name VARCHAR(255) NOT NULL,"
 					+ "    email VARCHAR(255) NOT NULL,"
 					+ "    password VARCHAR(255) NOT NULL)");
+	}
+
+	public void createChefTable() throws ClassNotFoundException, SQLException {
+		statement("CREATE TABLE users ("
+				+ "    uuid UUID DEFAULT gen_random_uuid() PRIMARY KEY,"
+				+ "    nome VARCHAR(255) NOT NULL,"
+				+ "    idade INT NOT NULL,"
+				+ "    ativo BOOLEAN NOT NULL)");
 	}
 }
