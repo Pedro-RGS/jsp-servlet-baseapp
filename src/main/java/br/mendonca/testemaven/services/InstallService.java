b6b0e81b802010fb4cc145b40c5231de218a9ab2
paulabeatrizz <paula.beatriz@upe.br>
2024-11-12 13:53:06 -0300

    fix:criação da tabela chef

--- src/main/java/br/mendonca/testemaven/services/InstallService.java
+++ src/main/java/br/mendonca/testemaven/services/InstallService.java
@@ -42,7 +42,7 @@ public class InstallService {
 	}
 
 	public void createChefTable() throws ClassNotFoundException, SQLException {
-		statement("CREATE TABLE chefs ("
+		statement("CREATE TABLE chef ("
 				+ "    uuid UUID DEFAULT gen_random_uuid() PRIMARY KEY,"
 				+ "    nome VARCHAR(255) NOT NULL,"
 				+ "    idade INT NOT NULL,"
